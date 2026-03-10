9798dbbd390db60f50516fd0933fb836da2cda86
thalissonmelo10 <thalissonmelo47@gmail.com>
2026-03-10 10:55:51 -0500

    novo

---
+++ script.js
@@ -0,0 +1,36 @@
+const lista = document.querySelector(".lista-produtos") //
+
+const cafe = [
+
+{
+    nome: "espresso",
+    descrição: "café concetrado y aromático",
+    preço: "$3.50",
+    img: "assents/produtos.jpg"
+},
+
+{ 
+    nome: "capuccino",
+    descrição: "espresso con lache vaporizada y espuma",
+    preço: "$4.50",
+    img: "assents/produtos.jpg"
+
+},
+
+{
+    nome:"cafe helado",
+    descrição:"refrescante café frio con hielo",
+    preço:"6.50"
+    img: "assents/produtos.jpg"
+},
+
+{
+    nome"lasãna",
+    descrição:"capas de pasta con carne y queso gratinado",
+    preço:"$8.00",
+
+}
+
+
+
+]
