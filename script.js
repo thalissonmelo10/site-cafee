f4586b025e08a1ed9a6acf7a572246b19a9f1896
thalissonmelo10 <thalissonmelo47@gmail.com>
2026-03-11 10:57:25 -0500

    atualizando

--- script.js
+++ script.js
@@ -1,36 +1,99 @@
-const lista = document.querySelector(".lista-produtos") //
+const lista = document.querySelector(".lista-produtos") // 
 
 const cafe = [
 
-{
-    nome: "espresso",
-    descrição: "café concetrado y aromático",
-    preço: "$3.50",
-    img: "assents/produtos.jpg"
-},
+    {
+        nome: "Espresso",
+        descricao: "Café concentrado y aromático",
+        proco: "3,50",
+        img: "/assents/espresso.jpg"
+    },
 
-{ 
-    nome: "capuccino",
-    descrição: "espresso con lache vaporizada y espuma",
-    preço: "$4.50",
-    img: "assents/produtos.jpg"
+    {
+        nome: "Cappucino",
+        descricao: "Espresso con leche vaporizada y espuma",
+        preco: "$4.50",
+        img: "/assents/cappuccino.jpg"
+    },
 
-},
-
-{
-    nome:"cafe helado",
-    descrição:"refrescante café frio con hielo",
-    preço:"6.50"
-    img: "assents/produtos.jpg"
-},
-
-{
-    nome"lasãna",
-    descrição:"capas de pasta con carne y queso gratinado",
-    preço:"$8.00",
-
-}
-
-
+    {
+        nome: "Café helado",
+        descricao: "Refrescante café frio con hielo",
+        preco: "$5.00",
+        img: "/assents/café helado.jpg"
+    }
 
 ]
+
+const comidas = [
+
+    {
+        nome: "Hamburguesa",
+        descricao: "Carne jugosa con queso y pan artesnal",
+        preco: "$15.00",
+        img: "/assents/hamburguer.webp"
+
+    },
+
+    {
+        nome: "Pizza",
+        descricao: "Pizza guestosa con mussarela",
+        preco: "$55.00",
+        img: "/assents/pizza.webp"
+
+    },
+
+    {
+        nome: "Risoto",
+        descricao: "risoto de camarono",
+        preco: "$19.00",
+        img: "/assents/risto.webp"
+
+    }
+
+]
+
+function mostrarProdutos(produtos){
+
+    lista.innerHTML = ""
+
+produtos.array.forEach(produto => {
+
+    lista.innerHTML += `
+
+<div class="produto">
+
+<img class="imagem-produto" src="${produto.img}" alt="">
+
+<div class="informacao-produto">
+
+<p class="titulo-produto">${produto.nome}</p>
+
+<p class="subtitulo-produto">${produto.descricao}</p>
+
+</div>
+
+<div class="preco-e-btn">
+
+<p class="preco-produto">${produto.preco}</p>
+
+<button class="btn-produto">
+
+<i class="bi bi-cart"></i>
+`
+
+})
+}
+
+document.querySelector(".btn02-produtos").addEventListener("click", () => {
+
+    mostrarProdutos(cafe)
+
+})
+
+
+document.querySelector(".btn03-produtos").addEventListener("click", () => {
+
+    mostrarProdutos(comidas)
+
+})
